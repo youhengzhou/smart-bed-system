d3b60619b1a80cb459497e9f162910d8f2fe62c1
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2021-10-27 20:56:29 -0400

    initialize the database

--- streamtest.py
+++ streamtest.py
@@ -1,8 +1,11 @@
+import datetime
 import jsonengine.main as eng
 
-i = 0
-eng.create({})
-while (1):
-    eng.patch({str(i):{'1':i}})
+eng.create({},'testdatabase1') # this creates a new database
+i = 0 # i is the database entry id
+sel = '' # the placeholder information we input
+while (sel != 'q' and 'quit' and 'exit'):
+    sel = input('\n> ')
+    eng.patch({str(i):{str(datetime.datetime.now()):str(sel)}},'testdatabase1')
     i += 1
     
